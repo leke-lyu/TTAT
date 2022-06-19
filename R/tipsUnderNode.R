011c98bc4848200fb7fdc934ab9c74aace02cfb7
Leke Lyu <lekelyu@compsups-iMac-3.local>
2022-06-18 21:54:18 -0400

    Start with two R functions

---
+++ R/tipsUnderNode.R
@@ -0,0 +1,27 @@
+#' Traverse the whole tree and return the proportion of traits under each internal nodes
+#' @import magrittr
+#' @import utils
+#'
+#' @param tree phylo
+#' @param nTip integer
+#' @param node integer
+#' @param tipsVector character
+#' @param ... further arguments passed to or from other methods
+#'
+#' @return Return a vector of character
+#' @export
+
+tipsUnderNode <- function(tree, node, nTip, tipsVector, ...){
+
+  for(j in tree$edge[tree$edge[,1] == node, 2]){
+    if(j > nTip){
+      tipsVector <- tipsUnderNode(tree, j, nTip, tipsVector)
+    }else{
+      tip <- j %>% tree$tip.label[.]
+      tipsVector <- c(tipsVector, tip)
+    }
+  }
+
+  return(tipsVector)
+
+}
